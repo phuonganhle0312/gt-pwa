6717d25a04bfe5dafe736e8487b2342797159d05
Phuong Le <phuonganh.le0312@gmail.com>
2020-08-12 00:27:55 -0400

    added db.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 console.log ("Service worker loaded!");
-const FILES_TO_CACHE = ["/", "/index.html", "index.js", "manifest.webmanifest", "service-worker.js","favicon.ico","/icons/icon-192x192.png", "/icons/icon-512x512.png", "styles.css"];
+const FILES_TO_CACHE = ["/", "/index.html", "db.js", "index.js", "manifest.webmanifest", "service-worker.js","favicon.ico","/icons/icon-192x192.png", "/icons/icon-512x512.png", "styles.css"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
